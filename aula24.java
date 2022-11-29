f9ad345571f07427b5f1686f6a00460460fd6f04
JoasGomes <jojoca.vitor777@gmail.com>
2022-11-29 13:08:58 -0300

    Herança e Protected

---
+++ aula24.java
@@ -0,0 +1,7 @@
+public class aula24 {
+    public static void main(String[] args) {
+        businessAccount account = new businessAccount();
+
+        account.deposit(0);
+    }
+}
---
+++ businessAccount.java
@@ -0,0 +1,27 @@
+public class businessAccount extends account2 {
+
+    private Double loanLimit;
+
+    public businessAccount() {
+
+    }
+
+    public businessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount) {
+        if (amount <= loanLimit) {
+            balance += amount - 10.0;
+        }
+    }
+}
