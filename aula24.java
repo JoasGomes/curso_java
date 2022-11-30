abc965b8ea95b3fb5e9906e40bb83e432ecb15f1
JoasGomes <jojoca.vitor777@gmail.com>
2022-11-30 17:04:37 -0300

    Upcasting e Downcasting

--- aula24.java
+++ aula24.java
@@ -1,7 +1,30 @@
 public class aula24 {
     public static void main(String[] args) {
-        businessAccount account = new businessAccount();
 
-        account.deposit(0);
+        account2 acc = new account2(1000, "joas", 0.0);
+        businessAccount bacc = new businessAccount(1001, "vitor", 0.0, 500.0);
+
+        // upcasting
+
+        account2 acc1 = bacc;
+        account2 acc2 = new businessAccount(1000, "joas", 0.0, 100.0);
+        account2 acc3 = new savingAccount(1004, "ana", 0.5, 0.05);
+
+        // downcasting
+
+        businessAccount acc4 = (businessAccount) acc2;
+        acc4.loan(100.0);
+
+        if (acc3 instanceof businessAccount) {
+            businessAccount acc5 = (businessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof savingAccount) {
+            savingAccount acc5 = (savingAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
     }
 }
---
+++ savingAccount.java
@@ -0,0 +1,25 @@
+public class savingAccount extends account2 {
+
+    private Double interestRate;
+
+    public savingAccount() {
+        super();
+    }
+
+    public savingAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance() {
+        balance += balance * interestRate;
+    }
+}
